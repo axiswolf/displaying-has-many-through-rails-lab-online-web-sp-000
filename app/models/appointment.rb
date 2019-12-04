fc9bafd4dbcd86d59e1f3c49e1ded7e64bcb0ba4
Karen V <karenvo@usc.edu>
2019-12-04 10:32:48 +0000

    Automatically backed up by Learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,4 +2,9 @@ class Appointment < ApplicationRecord
   belongs_to :patient
   belongs_to :doctor
 
+  def cor_date
+    time = self.appointment_datetime
+    cor_time = time.strftime
+  end
+  
 end
