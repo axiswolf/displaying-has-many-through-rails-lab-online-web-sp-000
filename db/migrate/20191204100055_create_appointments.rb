c88b58ec769a5a70184c957c4a24962921b44a8d
Karen V <karenvo@usc.edu>
2019-12-04 10:08:22 +0000

    Automatically backed up by Learn

--- db/migrate/20191204100055_create_appointments.rb
+++ db/migrate/20191204100055_create_appointments.rb
@@ -2,7 +2,8 @@ class CreateAppointments < ActiveRecord::Migration[5.0]
   def change
     create_table :appointments do |t|
       t.datetime :appointment_datetime
-      t.string :patient
+      t.string :patient_id
+      t.string :doctor_id
 
       t.timestamps
     end
