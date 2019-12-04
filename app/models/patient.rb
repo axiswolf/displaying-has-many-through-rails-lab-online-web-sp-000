e6c49a793c9d424b9eba1ba0741824bfb38ada78
Karen V <karenvo@usc.edu>
2019-12-04 10:53:12 +0000

    Automatically backed up by Learn

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,8 @@
 class Patient < ApplicationRecord
   has_many :appointments
   has_many :doctors, through: :appointments
+
+  def app_count
+    self.appointment.appointment_datetime.count
+  end
 end
